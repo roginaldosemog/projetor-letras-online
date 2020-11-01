b11c1dd7dc747633f71285b9bb323330da7c6cdb
Igor Aragão <roginaldosemog@gmail.com>
2020-11-01 03:23:53 -0300

    Adiciona router com páginas iniciais

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
+import Home from "./pages/Home";
+import Control from "./pages/Control";
+import Projection from "./pages/Projection";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/controle" component={Control} />
+        <Route path="/projetor" component={Projection} />
+      </Switch>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ src/pages/Projection.js
@@ -0,0 +1,9 @@
+export default function Control() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Projector Page</p>
+      </header>
+    </div>
+  );
+}
